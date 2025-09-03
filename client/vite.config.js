c8cb9156a2ebe47b81ef1bf78244b25ae9c23290
Ankush-verma-Source <ankushverma100009@gmail.com>
2025-09-03 22:06:45 +0530

    undo changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import legacy from "@vitejs/plugin-legacy";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
-  ],
-});
+  plugins: [react()],
+})
